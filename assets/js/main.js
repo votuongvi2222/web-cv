9a69656a207b6ab7fba86f8206cad2615e8fe870
Vo Tuong Vi <votuongvi2222002@gmail.com>
2021-11-29 18:03:32 +0700

    CV2 - style sidebar, upload function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 
 
 var loadFile = function(event) {
+  $('#cropper__container').show();
+
   var image = document.getElementById('image--cropped');
   var review = document.getElementById('preview')
   console.log('changed')
@@ -14,7 +16,7 @@ var loadFile = function(event) {
   console.log(image.src)
   new Cropper(image, {
     // dragMode: DRAG_MODE_CROP,
-    aspectRatio: 3/4,
+    aspectRatio: 1/1,
     preview: review,
     crop(event) {
       console.log(event.detail.x);
@@ -64,3 +66,64 @@ $(document).ready(function(){
   });
   console.log(sample_name_content);
 });
+
+$(document).ready(function () {
+  // toolbar
+  $('.toolbar-btn').click(function(){
+    $(this).toggleClass('focused');
+  })
+  // editor 
+  $('#post_editor-expand').hide();
+
+  $('#post_content_editor-textarea').focus(function(){
+    $('#editor_title-textarea').addClass('focused');
+    $('.content_editor_focus-line').addClass('focused');
+  
+  })
+  $('#post_content_editor-textarea').blur(function(){
+    var textboxContent = $.trim($('#post_content_editor-textarea').text());
+    if(textboxContent == '')
+      $('#editor_title-textarea').removeClass('focused');
+      $('.content_editor_focus-line').removeClass('focused');
+
+  })
+  $('body').on('DOMSubtreeModified', '#post_content_editor-textarea', function(){
+    // console.log('changed');
+    var textboxContent = $.trim($('#post_content_editor-textarea').text());
+    if(textboxContent != '')
+      $('#upload_post-tab').removeClass('empty');
+    else
+      $('#upload_post-tab').addClass('empty');
+  })
+  $('#cancel_post-tab').click(function(){
+    $('#post_editor-expand').hide();
+    $('#post_editor-btn').show();
+    $('#post_content_editor-textarea').innerText = '';
+  })
+  $('#post_editor-btn').click(function(){
+    $('#post_editor-expand').show();
+    $('#post_editor-btn').hide();
+    $('#post_content_editor-textarea').focus();
+  })
+
+  $('#file').click(() => {
+    $('.avatar__box--left').removeClass('hide');
+    var avatarW = $('.avatar__box--left').outerWidth();
+    var cvHeaderW = $('.cv__header').outerWidth();
+    console.log('avaw: ' + avatarW);
+    console.log('cvh: ' + cvHeaderW);
+    $('.cv__opening--right').width(cvHeaderW - avatarW - 4);
+  })
+
+  $('#remove__btn').click(() => {
+    $('.avatar__box--left').addClass('hide');
+    $('#cropper__container').hide();
+    var cvHeaderW = $('.cv__header').outerWidth();
+    console.log('cvh: ' + cvHeaderW);
+    $('.cv__opening--right').width(cvHeaderW);
+  })
+
+});
+
+
+
