7a1407358cd515d07541d8005ba3ed22741b230a
Vo Tuong Vi <votuongvi2222002@gmail.com>
2021-11-30 17:46:00 +0700

    Updat crop img fuc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,39 @@
 // import Cropper from 'cropperjs';
+// GG SIGN IN
+var ggID = localStorage.getItem('uid'),
+    ggAvatar = localStorage.getItem('avatar'),
+    ggEmail = localStorage.getItem('email'),
+    ggName = localStorage.getItem('name');
 
-
-var loadFile = function(event) {
-  $('#cropper__container').show();
-
-  var image = document.getElementById('image--cropped');
-  var review = document.getElementById('preview')
-  console.log('changed')
-	image.src = URL.createObjectURL(event.target.files[0]);
-  // var cropperCanva = $('.cropper-canvas img')[0];
-  // cropperCanva.src = URL.createObjectURL(event.target.files[0]);
-  $.each($("img"), function() {
-    $(this).attr("src", URL.createObjectURL(event.target.files[0]));
-  });
-  console.log(image.src)
-  new Cropper(image, {
-    // dragMode: DRAG_MODE_CROP,
-    aspectRatio: 1/1,
-    preview: review,
-    crop(event) {
-      console.log(event.detail.x);
-      console.log(event.detail.y);
-      console.log(event.detail.width);
-      console.log(event.detail.height);
-      console.log(event.detail.rotate);
-      console.log(event.detail.scaleX);
-      console.log(event.detail.scaleY);
-    },
-  });
-};
-
-$(document).ready(function(){
-    
-  let sample_name_content =  $(".employee_name").text();;
-  let sample_profession_name_content = $(".profession_name").text();
-
-  $(".employee_name").focus(function(){
-      $(".employee_name").empty();
-  });
-
-  $(".profession_name").focus(function(){
-      $(".profession_name").empty();
-  });
-
-  $("#experience--save").on('click',function(){
-      let started_date = $("#experience--from").val();
-      let end_date = $("#experience--to").val();
-      let company = $("#company--name").val();
-      let task = $("#task").val();
-      let descript = $("#job--description").val();
-      $('.modal-body').find('input').each(function(){
-          if(!$(this).prop('required')){
-              console.log("NR");
-          } else {
-              if(!$(this).val()){
-                  $(".alert").css("display","block");
-                  return false;
-              }
-                  $('#work--experience').append("<li><div class='date'> From " + started_date + " to " + end_date +
-                  "</div> <div class='info'><p class='semi-bold'>"+ task +" ( for " + company +")"+
-                  "</p> <p>" + descript+"</p></div></li>");
-                  $("#addExperience").modal("hide");
-          }
-      })
-  });
-  console.log(sample_name_content);
-});
+var image = document.getElementById('image--cropped');  
+image.src = ggAvatar;
+var options = {
+  aspectRatio: 1/1,
+  preview: '#preview',
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  }
+}
+var cropper = new Cropper(image, options);
+var uploadedImageURL;
 
 $(document).ready(function () {
+  $('#cropper__container').show();
+  $('#cropper__container').height(200);
+
+  $('.avatar__box--left').removeClass('hide');
+
+  var avatarW = $('.avatar__box--left').outerWidth();
+  var cvHeaderW = $('.cv__header').outerWidth();
+  console.log('avaw: ' + avatarW);
+  console.log('cvh: ' + cvHeaderW);
+  $('.cv__opening--right').width(cvHeaderW - avatarW - 4);
   // toolbar
   $('.toolbar-btn').click(function(){
     $(this).toggleClass('focused');
@@ -129,12 +95,75 @@ $(document).ready(function () {
   $('#remove__btn').click(() => {
     $('.avatar__box--left').addClass('hide');
     $('#cropper__container').hide();
+
     var cvHeaderW = $('.cv__header').outerWidth();
     console.log('cvh: ' + cvHeaderW);
     $('.cv__opening--right').width(cvHeaderW);
   })
 
 });
+var inputImage = document.getElementById('file');
+var loadFile = function(event) {
+  // $('.cropper-container').width(0);
+  // $('.cropper-container').height(0);
+  $('#cropper__container').show();
+  $('#cropper__container').height(200);
+
+  // var image = document.getElementById('image--cropped');
+  // var review = document.getElementById('preview')
+  // console.log('changed')
+	image.src = URL.createObjectURL(event.target.files[0]) ;
+  if (cropper) {
+    cropper.destroy();
+  }
+
+  cropper = new Cropper(image, options);
+  inputImage.value = null;
+  // var cropperCanva = $('.cropper-canvas img')[0];
+  // cropperCanva.src = URL.createObjectURL(event.target.files[0]);
+  // $.each($("img"), function() {
+  //   $(this).attr("src", URL.createObjectURL(event.target.files[0])) ;
+  // });
+};
+
+$(document).ready(function(){
+    
+  let sample_name_content =  $(".employee_name").text();;
+  let sample_profession_name_content = $(".profession_name").text();
+
+  $(".employee_name").focus(function(){
+      $(".employee_name").empty();
+  });
+
+  $(".profession_name").focus(function(){
+      $(".profession_name").empty();
+  });
+
+  $("#experience--save").on('click',function(){
+      let started_date = $("#experience--from").val();
+      let end_date = $("#experience--to").val();
+      let company = $("#company--name").val();
+      let task = $("#task").val();
+      let descript = $("#job--description").val();
+      $('.modal-body').find('input').each(function(){
+          if(!$(this).prop('required')){
+              console.log("NR");
+          } else {
+              if(!$(this).val()){
+                  $(".alert").css("display","block");
+                  return false;
+              }
+                  $('#work--experience').append("<li><div class='date'> From " + started_date + " to " + end_date +
+                  "</div> <div class='info'><p class='semi-bold'>"+ task +" ( for " + company +")"+
+                  "</p> <p>" + descript+"</p></div></li>");
+                  $("#addExperience").modal("hide");
+          }
+      })
+  });
+  console.log(sample_name_content);
+});
+
+
 
 
 
